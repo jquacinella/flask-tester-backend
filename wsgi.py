865191858503def973c8574eadae3b8d8d451b2c
James Quacinella <james@deemok.com>
2016-11-29 18:12:58 -0500

    Simple Flask App to serve up text

---
+++ wsgi.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+application = Flask(__name__)
+
+@application.route('/', methods=['GET'])
+def index():
+    return 'Hello world!'
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
