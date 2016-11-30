a474fd622d1673b518892b31efe5665f71b3b9e6
James Quacinella <james@deemok.com>
2016-11-29 22:13:23 -0500

    Adding redis counter in api call

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,16 @@
 from flask import Flask
 application = Flask(__name__)
 
+import redis
+rdb = redis.Redis('redis')
+#rdb.set("counter", 0)
+
 @application.route('/', methods=['GET'])
 def index():
-    return 'Hello world!'
+    rdb.incr("counter")
+    counter = rdb.get("counter")
+    
+    return 'Counter: {}\n'.format(counter)
 
 
 if __name__ == '__main__':
